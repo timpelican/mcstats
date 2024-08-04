2376fa9ee8aefe7eb48dae56b5824eb700e3dbc9
Tim Franklin <tim@pelican.org>
2024-08-04 21:09:15 +0100

    User login / logout.  Restrict some pages to logged in users.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
 from app import app, db
 from app.forms import LoginForm, VillainForm, HeroForm
-from app.models import Phase, Aspect
+from app.models import Phase, Aspect, User
+from urllib.parse import urlsplit
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,14 +20,29 @@ def stats():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/villain', methods=['GET', 'POST'])
+@login_required
 def villain():
     form = VillainForm()
     #TODO: Need an actual error when no Phases are defined.
@@ -36,6 +55,7 @@ def villain():
     return render_template('villain.html', title='Villain', form=form)
 
 @app.route('/hero', methods=['GET', 'POST'])
+@login_required
 def hero():
     form = HeroForm()
     #TODO: Need an actual error when no Phases are defined.
