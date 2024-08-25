732577c62d07b95bc2e57ab21cdd4464711b58cb
Tim Franklin <tim@pelican.org>
2024-08-25 16:23:29 +0100

    pylint clean-up on models.

--- app/models.py
+++ app/models.py
@@ -1,16 +1,17 @@
 from typing import Optional
+import enum
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-import enum
+from app import db, login
 
 @login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
 
 class User(UserMixin, db.Model):
+    """User of the application"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -19,15 +20,16 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)    
+        return check_password_hash(self.password_hash, password)
 
 class Phase(db.Model):
+    """Phases in which the game was released"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     phasename: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -36,30 +38,33 @@ class Phase(db.Model):
     heroes: so.Mapped[list['Hero']] = so.relationship(back_populates='phase')
 
     def __repr__(self):
-        return '<Phase {}>'.format(self.phasename)
-    
+        return f'<Phase {self.phasename}>'
+
 class Villain(db.Model):
+    """Villains"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                             unique=True)
     phase_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Phase.id),
                                                index=True)
-    
+
     phase: so.Mapped[Phase] = so.relationship(back_populates='villains')
 
     def __repr__(self):
-        return '<Villain {}>'.format(self.name)
-    
+        return f'<Villain {self.name}>'
+
 class Aspect(db.Model):
+    """Deck-building Aspects"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                             unique=True)
     heroes: so.Mapped[list['Hero']] = so.relationship(back_populates='default_aspect')
 
     def __repr__(self):
-        return '<Aspect {}>'.format(self.name)
+        return f'<Aspect {self.name}>'
 
 class Hero(db.Model):
+    """Heroes"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                             unique=True)
@@ -71,24 +76,26 @@ class Hero(db.Model):
     default_aspect: so.Mapped[Aspect] = so.relationship(back_populates='heroes')
 
     def __repr__(self):
-        return '<Hero {}>'.format(self.name)
+        return f'<Hero {self.name}>'
 
     def result_vs_villain(self, villain):
         return db.session.execute(sa.select(Result).where(Result.hero_id == self.id).
                                   where(Result.villain_id == villain.id)).scalar()
-    
+
     def result_as_cell(self, villain):
         r = self.result_vs_villain(villain)
         if r is None:
             return "<td></td>"
         else:
             return r.as_cell()
-        
+
 class ResultTypes(enum.Enum):
-    win = 1
-    loss = 2
+    """Simple enum for possible results"""
+    WIN = 1
+    LOSS = 2
 
 class Result(db.Model):
+    """Pairing of Villain and Hero"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     hero_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Hero.id),
                                                index=True)
@@ -99,14 +106,14 @@ class Result(db.Model):
     villain: so.Mapped[Villain] = so.relationship()
 
     def __repr__(self):
-        return '<Result {} vs {}: {}>'.format(self.hero.name, self.villain.name, self.result)
+        return f'<Result {self.hero.name} vs {self.villain.name}: {self.result}>'
 
     def as_cell(self):
         #TODO: this should return some kind of CSS selector
         match self.result:
-            case ResultTypes.win:
+            case ResultTypes.WIN:
                 return '<td bgcolor="#00ff00">W</td>'
-            case ResultTypes.loss:
+            case ResultTypes.LOSS:
                 return '<td bgcolor="#ff0000">L</td>'
             case _:
                 return '<td></td>'
