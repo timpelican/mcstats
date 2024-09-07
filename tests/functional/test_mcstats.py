850aea3cde945636e159d34b2a7e2184b394327f
Tim Franklin <tim@pelican.org>
2024-09-07 21:41:05 +0100

    Add test coverage check.

--- tests/functional/test_mcstats.py
+++ tests/functional/test_mcstats.py
@@ -1,6 +1,10 @@
+"""
+Top-level functional tests for the MCStats application
+"""
+
 import os
 os.environ['DATABASE_URL'] = 'sqlite://'
-from app import app, db
+#from app import app, db
 
 def test_home_page(test_client):
     """
@@ -8,10 +12,13 @@ def test_home_page(test_client):
     WHEN the '/' page is requested (GET)
     THEN check the response is valid
     """
-    
+
     response = test_client.get('/')
     assert response.status_code == 200
     assert b'Welcome to Marvel Champions Stats' in response.data
+    # Test client is not logged in - we should see login link, but no logout
+    assert b'login' in response.data
+    assert b'logout' not in response.data
 
 def test_home_page_post(test_client):
     """
